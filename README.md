63ccb6df5e73d5dd2dfe0b7163b68fa645f81f0a
MarcosMaldonado <22524098+naraaoi@users.noreply.github.com>
2023-02-05 19:01:46 -0300

    el 'piedra, papel o tijera' se encuentra funcional para jugar en la consola del navegador

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PiedraPapelOTijeras
+
 Actividad realizada para el curso fullstack: TheOdinProject.com
+
+Para jugar en la consola del navegador (?) por ahora.
--- main.js
+++ main.js
@@ -51,22 +51,109 @@ if (humano === "tijera" && pc === "piedra"){
 
 //Resultado del enfrentamiento contra la pc
     
-    return console.log("---------------" + "\nElegiste: " + humano, "\nTu rival eligio: " + pc, "\nResultado: " + resultado + "\n---------------");
+return console.log("---------------" + "\nElegiste: " + humano, "\nTu rival eligio: " + pc, "\nResultado: " + resultado + "\n---------------");
 
+
+}
+
+function puntaje(humano, pc){
+
+    //Mismo If, pero para puntaje, deberia ver si hay forma de hacer todo en un solo if
+
+//en el caso de elegir piedra
+let puntaje = 0;
+
+if (humano === "piedra" && pc === "piedra"){
+    puntaje = puntaje + 1;
+} else if (humano === "piedra" && pc === "papel"){
+    puntaje = puntaje - 1;
+} else if (humano === "piedra" && pc === "tijera"){
+    puntaje = puntaje + 2;
+}
+
+//en el caso de elegir papel
+
+if (humano === "papel" && pc === "piedra"){
+    puntaje = puntaje + 2;
+} else if (humano === "papel" && pc === "papel"){
+    puntaje = puntaje + 1;
+} else if (humano === "papel" && pc === "tijera"){
+    puntaje = puntaje - 1;
+}
+
+//en el caso de elegir tijera
+
+if (humano === "tijera" && pc === "piedra"){
+    puntaje = puntaje -1;
+} else if (humano === "tijera" && pc === "papel"){
+    puntaje = puntaje + 2;
+} else if (humano === "tijera" && pc === "tijera"){
+    puntaje = puntaje +1;
+}
+
+return puntaje;
 }
 
 function game(humano, pc,){
 
-    let puntajehum = 0;
- 
-    for (let i = 0; i < 5; i++) {
-        (ronda(humano, pc));
-        console.log(i);
+    let puntajeh = 0;
+    let puntajepc = 0;
 
-        console.log("Puntaje: " + puntajehum);
+    for (let i = 0; i < 4; i++) {
+
+        ronda(humano, pc);
+
+        if (puntaje(humano,pc) === 2){
+            puntajeh = puntajeh + 2;
+        }else if (puntaje(humano,pc) === 1){
+            puntajeh = puntajeh + 1;
+        }else if (puntaje(humano,pc) === -1){
+            puntajeh = puntajeh; 
+        }
+
+        if (puntaje(humano,pc) === 2){
+            puntajepc = puntajepc;
+        }else if (puntaje(humano,pc) === 1){
+            puntajepc = puntajepc + 1;
+        }else if (puntaje(humano,pc) === -1){
+            puntajepc = puntajepc +2; 
+        }
+
+        console.log(`Tu puntuación:${puntajeh}\n`);
+        console.log(`Puntuación de pc:${puntajepc}`);
         humano = eleccionHumano();
         pc = eleccionCPU();
+        
     }
+
+    ronda(humano, pc);
+
+    if (puntaje(humano,pc) === 2){
+        puntajeh = puntajeh + 2;
+    }else if (puntaje(humano,pc) === 1){
+        puntajeh = puntajeh + 1;
+    }else if (puntaje(humano,pc) === -1){
+        puntajeh = puntajeh; 
+    }
+
+    if (puntaje(humano,pc) === 2){
+        puntajepc = puntajepc;
+    }else if (puntaje(humano,pc) === 1){
+        puntajepc = puntajepc + 1;
+    }else if (puntaje(humano,pc) === -1){
+        puntajepc = puntajepc +2; 
+    }
+    console.log(`Tu puntuación:${puntajeh}`);
+    console.log(`Puntuación de pc:${puntajepc}`);
+
+if (puntajeh > puntajepc){
+    console.log("GANASTE!")
+} else if (puntajeh === puntajepc){
+    console.log("Empate.")
+}else if (puntajeh < puntajepc){
+    console.log("Perdiste.")
+}
+
 }
 
 let hum = eleccionHumano();
